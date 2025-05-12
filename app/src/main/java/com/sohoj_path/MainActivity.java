91c3ab79036294e2ea0575d4ca108577143531dd
bengalbee25 <shazzatulislamanam@gmail.com>
2025-05-12 23:05:33 +0600

    initial commit

--- app/src/main/java/com/sohoj_path/MainActivity.java
+++ app/src/main/java/com/sohoj_path/MainActivity.java
@@ -3,6 +3,7 @@ package com.sohoj_path;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,14 +12,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
-    Button button;
+    //Button button;
     TextView textView;
+    NavigationView navigationView;
     FirebaseUser user;
 
     @Override
@@ -31,9 +35,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
+        navigationView = findViewById(R.id.navigationView);
+        View headerView = navigationView.getHeaderView(0);
+        textView = headerView.findViewById(R.id.user_details);
+        //button = findViewById(R.id.logout);
+        //textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
         if (user == null)
         {
@@ -44,15 +53,24 @@ public class MainActivity extends AppCompatActivity {
         else {
             textView.setText(user.getEmail());
         }
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//                Intent intent = new Intent(getApplicationContext(), login.class);
+//                startActivity(intent);
+//                finish();
+//
+//            }
+//        });
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.logout) {
                 FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), login.class);
-                startActivity(intent);
+                startActivity(new Intent(getApplicationContext(), login.class));
                 finish();
-
+                return true;
             }
+            return false;
         });
     }
 }
