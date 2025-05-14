083d44fc64a51cb371eff1d44f35e48cb4c69065
bengalbee25 <shazzatulislamanam@gmail.com>
2025-05-14 19:28:45 +0600

    initial commit

--- app/src/main/java/com/sohoj_path/AddFragment.java
+++ app/src/main/java/com/sohoj_path/AddFragment.java
@@ -1,6 +1,7 @@
 package com.sohoj_path;
 
 import android.os.Bundle;
+import android.content.Intent;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,6 +14,13 @@ public class AddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        if (getActivity() != null) {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            getActivity().finish();
+        }
+
+        return new View(requireContext());
     }
 }
